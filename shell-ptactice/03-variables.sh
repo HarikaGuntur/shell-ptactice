2bb5dc8dadc4df16f0390e70589518eec493b441
Harika Guntur <hrknd4@gmail.com>
2026-03-03 11:32:12 +0530

    actions

--- shell-ptactice/03-variables.sh
+++ shell-ptactice/03-variables.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-person1=Suresh 
-person2=Ramesh
+person1=Trump 
+person2=putin
 echo "$person1:Hello $person2,How are you?"
 echo "$person2:Hi $person1,I am fine thanks for asking how are you doing?"
 echo "$person1:doing fine.what are you learning $person2?"
