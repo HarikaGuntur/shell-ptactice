94c3cd81481a216568c26041b28d0aa884c1e5be
Harika Guntur <hrknd4@gmail.com>
2026-03-03 11:27:17 +0530

    fixed structure

---
+++ shell-ptactice/03-variables.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+person1=Suresh 
+person2=Ramesh
+echo "$person1:Hello $person2,How are you?"
+echo "$person2:Hi $person1,I am fine thanks for asking how are you doing?"
+echo "$person1:doing fine.what are you learning $person2?"
+echo "$person2:I am learning shell.what about you?"
