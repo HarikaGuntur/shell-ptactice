735419cdbce7e59b630b86bad6408a03465ebac6
Harika Guntur <hrknd4@gmail.com>
2026-03-04 10:44:55 +0530

    added 04 variables script

---
+++ 04-variables.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+person1=$1
+person2=$2
+echo "$person1:Hello $person2,How are you?"
+echo "$person2:Hi $person1,I am fine thanks for asking how are you doing?"
+echo "$person1:doing fine.what are you learning $person2?"
+echo "$person2:I am learning shell.what about you?"
