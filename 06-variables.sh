b871854667977ceaa90c7fa09164d853e611d726
Harika Guntur <hrknd4@gmail.com>
2026-03-04 12:07:55 +0530

    changed

--- 06-variables.sh
+++ 06-variables.sh
@@ -1,5 +1,12 @@
 #!/bin /bash
 
-timestamp=$(date)
+START_TIME=$(date +%s)
 
 echo "script executed at :$timestamp"
+
+sleep 10
+
+END_TIME=$(date +%s)
+TOTAL_TIME=$(($START_TIME-$END_TIME))
+
+echo "script executed at :$TOTAL_TIME"
