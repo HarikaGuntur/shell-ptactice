f44cb3337a994a713b719d82f3ed0c9822daf6d0
Harika Guntur <hrknd4@gmail.com>
2026-03-05 11:48:40 +0530

    new file added

--- 08-datatypes.sh
+++ 08-datatypes.sh
@@ -11,3 +11,7 @@ echo "sum is :$sum"
 fruits=("apple","bananna","pomo")
 
 echo "fruits are :${fruits[@]}"
+#indexing
+echo "fruits are :${fruits[0]}"
+echo "fruits are :${fruits[1]}"
+echo "fruits are :${fruits[2]}"
